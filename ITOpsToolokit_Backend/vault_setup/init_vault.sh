807262e9f7406af49e0ce6ec07175a6bfe92d15a
Kushalgowda06 <kushalgowda.s@cognizant.com>
2025-10-21 17:35:50 +0530

    Update init_vault.sh

--- ITOpsToolokit_Backend/vault_setup/init_vault.sh
+++ ITOpsToolokit_Backend/vault_setup/init_vault.sh
@@ -1,13 +1,31 @@
-ui = true
+#!/bin/bash
 
-storage "file" {
-  path = "/opt/vault/data"
-}
+export VAULT_ADDR='https://vault.local:8200'
+export VAULT_CACERT='/opt/vault/tls/tls.crt'
 
-listener "tcp" {
-  address = "0.0.0.0:8200"
-  tls_cert_file = "/opt/vault/tls/tls.crt"
-  tls_key_file = "/opt/vault/tls/tls.key"
-}
+echo "Initializing Vault..."
+vault operator init -key-shares=5 -key-threshold=3 > /opt/vault/init.txt
 
-disable_mlock = true
+# Extract unseal keys and root token
+keys=$(grep 'Unseal Key' /opt/vault/init.txt | awk '{print $NF}')
+token=$(grep 'Initial Root Token' /opt/vault/init.txt | awk '{print $NF}')
+
+echo "Unsealing Vault..."
+i=0
+for key in $keys; do
+  vault operator unseal $key
+  i=$((i+1))
+  [ $i -eq 3 ] && break
+done
+
+echo "Logging in with root token..."
+vault login $token
+
+echo "Enabling KV secrets engine..."
+vault secrets enable -path=secret kv
+
+echo "Adding secrets..."
+vault kv put secret/Platform_SNOW snow_user=your_user snow_password=your_pass
+vault kv put secret/cfs_problem_tickets db_user=db_user db_password=db_pass
+
+echo "Vault initialization complete."
