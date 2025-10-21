f3887f1b0deba9b636299f64a9057f768ae38cee
Kushalgowda06 <kushalgowda.s@cognizant.com>
2025-10-21 17:35:03 +0530

    Update init_vault.sh

--- ITOpsToolokit_Backend/vault_setup/init_vault.sh
+++ ITOpsToolokit_Backend/vault_setup/init_vault.sh
@@ -1,1 +1,13 @@
-CMD ["/bin/bash", "-c", "vault server -config=/etc/vault.d/vault.hcl & sleep 5 && init_vault.sh"]
+ui = true
+
+storage "file" {
+  path = "/opt/vault/data"
+}
+
+listener "tcp" {
+  address = "0.0.0.0:8200"
+  tls_cert_file = "/opt/vault/tls/tls.crt"
+  tls_key_file = "/opt/vault/tls/tls.key"
+}
+
+disable_mlock = true
