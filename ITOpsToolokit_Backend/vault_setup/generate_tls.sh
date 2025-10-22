496310457cf171e6047617f9adcbae8f249a2666
Kushalgowda06 <kushalgowda.s@cognizant.com>
2025-10-22 15:01:31 +0530

    Update generate_tls.sh

--- ITOpsToolokit_Backend/vault_setup/generate_tls.sh
+++ ITOpsToolokit_Backend/vault_setup/generate_tls.sh
@@ -4,6 +4,6 @@ mkdir -p vault_setup/tls vault_setup/data
 openssl req -out vault_setup/tls/tls.crt \
   -new -keyout vault_setup/tls/tls.key \
   -newkey rsa:4096 -nodes -sha256 -x509 \
-  -subj "/O=YourCompany/CN=vault.local" \
-  -addext "subjectAltName = IP:127.0.0.1,DNS:vault.local" \
+  -subj "/O=Cognizant/CN=RBAServer" \
+  -addext "subjectAltName = IP:0.0.0.0,DNS:rba.cognizantgoc.com" \
   -days 365
