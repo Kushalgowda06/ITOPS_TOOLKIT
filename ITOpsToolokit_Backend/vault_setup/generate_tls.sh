16b26de510a585c9a7f5b0d92604d545b1c9206b
Kushalgowda06 <kushalgowda.s@cognizant.com>
2025-10-22 21:48:24 +0530

    Update generate_tls.sh

--- ITOpsToolokit_Backend/vault_setup/generate_tls.sh
+++ ITOpsToolokit_Backend/vault_setup/generate_tls.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
-
 CERT_DIR="/opt/vault/tls"
 mkdir -p $CERT_DIR
 cd $CERT_DIR
 
-# Generate self-signed certificate
+# Use EC2 public IP and localhost in SANs
+EC2_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+
 openssl req -out tls.crt -new -keyout tls.key -newkey rsa:4096 -nodes -sha256 -x509 \
   -subj "/O=Company/CN=VaultServer" \
-  -addext "subjectAltName = IP:0.0.0.0,DNS:vault.local" \
+  -addext "subjectAltName = IP:${EC2_IP},IP:127.0.0.1,DNS:localhost" \
   -days 365
 
 chown -R 1000:1000 $CERT_DIR
