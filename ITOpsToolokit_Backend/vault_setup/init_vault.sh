a9aedcda958267e9a9366ac290ff84b443cf66ba
Kushalgowda06 <kushalgowda.s@cognizant.com>
2025-10-22 12:58:23 +0530

    Update init_vault.sh

--- ITOpsToolokit_Backend/vault_setup/init_vault.sh
+++ ITOpsToolokit_Backend/vault_setup/init_vault.sh
@@ -9,7 +9,5 @@ vault status || vault operator init -key-shares=5 -key-threshold=3 > /vault/init
 grep 'Unseal Key' /vault/init.txt | head -n 3 | awk '{print $NF}' | while read key; do
   vault operator unseal "$key"
 done
-
 # Save root token
 grep 'Initial Root Token' /vault/init.txt | awk '{print $NF}' > /vault/.vault_token
-``
