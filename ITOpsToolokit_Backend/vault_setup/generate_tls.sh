888bd7d6f376ee37787d5b1d257cad2cc26c7da9
Kushalgowda06 <kushalgowda.s@cognizant.com>
2025-10-22 18:30:33 +0530

    Update generate_tls.sh

--- ITOpsToolokit_Backend/vault_setup/generate_tls.sh
+++ ITOpsToolokit_Backend/vault_setup/generate_tls.sh
@@ -1,9 +1,14 @@
-#!/bin/bash
-mkdir -p vault_setup/tls vault_setup/data
+#!/bin/sh
+# Generates TLS certs for Vault
+set -e
 
-openssl req -out vault_setup/tls/tls.crt \
-  -new -keyout vault_setup/tls/tls.key \
+mkdir -p /vault/certs
+
+# Self-signed certificate
+openssl req -out /vault/certs/tls.crt -new -keyout /vault/certs/tls.key \
   -newkey rsa:4096 -nodes -sha256 -x509 \
-  -subj "/O=Cognizant/CN=RBAServer" \
+  -subj "/O=cognizant/CN=RBAServer" \
   -addext "subjectAltName = IP:0.0.0.0,DNS:rba.cognizantgoc.com" \
   -days 365
+
+chmod 644 /vault/certs/tls.crt /vault/certs/tls.key
