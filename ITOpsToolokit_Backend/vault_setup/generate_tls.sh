88df9ac3844a65a928b5c42ef89b86dc4b77423b
Kushalgowda06 <kushalgowda.s@cognizant.com>
2025-10-24 13:57:26 +0530

    Update generate_tls.sh

--- ITOpsToolokit_Backend/vault_setup/generate_tls.sh
+++ ITOpsToolokit_Backend/vault_setup/generate_tls.sh
@@ -5,15 +5,16 @@ CERT_DIR="/opt/vault/tls"
 mkdir -p "$CERT_DIR"
 cd "$CERT_DIR"
 
-# Use private IP directly
-EC2_IP="172.31.17.17"
-echo "Using IP: $EC2_IP for TLS certificate"
+# Use the server's private IP directly
+SERVER_IP="172.31.17.17"
 
-# Generate self-signed certificate with valid SAN
+echo "Using IP: $SERVER_IP for TLS certificate"
+
+# Generate self-signed certificate with SAN
 openssl req -x509 -newkey rsa:4096 -sha256 -days 365 -nodes \
   -keyout tls.key -out tls.crt \
   -subj "/O=Company/CN=VaultServer" \
-  -addext "subjectAltName = IP:${EC2_IP},IP:127.0.0.1,DNS:localhost"
+  -addext "subjectAltName = IP:${SERVER_IP},IP:127.0.0.1,DNS:localhost"
 
 chown -R 1000:1000 "$CERT_DIR"
 
